6309d35722d3378656c9d13bc82074def3661e95
John U <arjorb@gmail.com>
2023-02-27 16:53:19 +0200

    removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
-
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import Editor from './components/Editor';
-import { data } from './data';
 import Split from 'react-split';
 import 'react-mde/lib/styles/css/react-mde-all.css';
 import { nanoid } from 'nanoid';
@@ -40,18 +38,6 @@ export default function App() {
       return newArray;
     });
   }
-
-  /**
-   * Challenge: complete and implement the deleteNote function
-   *
-   * Hints:
-   * 1. What array method can be used to return a new
-   *    array that has filtered out an item based
-   *    on a condition?
-   * 2. Notice the parameters being based to the function
-   *    and think about how both of those parameters
-   *    can be passed in during the onClick event handler
-   */
 
   function deleteNote(event, noteId) {
     event.stopPropagation();
