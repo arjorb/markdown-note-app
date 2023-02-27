45406cfd7378b90c73792ada55848d585ef6cef4
John U <arjorb@gmail.com>
2023-02-27 16:48:28 +0200

    done fixing the md npm problem

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sidebar from './components/Sidebar';
 import Editor from './components/Editor';
 import { data } from './data';
 import Split from 'react-split';
+import 'react-mde/lib/styles/css/react-mde-all.css';
 import { nanoid } from 'nanoid';
 
 export default function App() {
